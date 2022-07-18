c8c54a715eed1d641480ba520f97974e1725ee99
NORIKOk <mun.7781.gon@gmail.com>
2022-07-18 19:08:15 +0900

    コントローラーとルーティングを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,27 @@
 class ItemsController < ApplicationController
-  # ログインしていないユーザーをログインページの画面に促すことができる。
- # before_action :authenticate_user!, only: :new
+ before_action :authenticate_user!, only: :new
 
- # def new
- # end
+ def index
+  @items = Item.all.order('created_at DESC')
+end
 
-  #  def update
-  #  end
+def new
+  @item = Item.new
+end
 
-  #  private
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
 
-  #  def user_params
-  #    params.require(:user).permit(:name, :email)
-  #  end
+private
+
+def item_params
+  params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id, :delivery_money_id, :prefecture_id, :sending_day_id,
+                               :price).merge(user_id: current_user.id)
+  end
 end
