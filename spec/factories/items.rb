7dd597da9a26cd737cad20940ce864315904f913
NORIKOk <mun.7781.gon@gmail.com>
2022-07-20 17:39:08 +0900

    正常系の記述と異常系の準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,22 +1,37 @@
 FactoryBot.define do
   factory :item do
-    # イメージ
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
+    #id
+    id                  {1} 
+
+    #user_id
+    user_id             {1}
+ 
+    #イメージ
+    image {Faker::Lorem.sentence}
+    
     # 商品名
-    name                { Faker::Commerce.product_name}
+    item_name                { Faker::Commerce.product_name}
+    
     # 商品の説明
+    explanation                {Faker::Commerce.department}
 
     # カテゴリー
+    category_id         {1}
 
     # 商品の状態
+    status_id           {1}
 
     # 配送料の負担
+    delivery_charge_id   {100}
 
     # 販売元の地域
+    prefecture_id       {1}
 
     # 発送までの日数
+    sending_day_id     {1}
 
     # 価格
+    price             {300}
   end
 end
 FactoryBot.define do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,15 +11,96 @@ RSpec.describe Item, type: :model do
     # 正常テスト
     context '出品できるとき' do
       it '出品画像が投稿できるとき' do
-        @item.image = fixture_file_upload('app/assets/images/logo.png')
+        @item.image
         expect(@item).to be_valid
-      end
+       end
+
+     it '商品名が入力される' do
+       @item.item_name
+     end
+
+     it '商品の説明が入力される' do
+       @item.explanation
+       expect(@item).to be_valid
+     end
+
+     # 商品の詳細
+     it  '商品のカテゴリーが選択される' do
+       @item.category_id
+       expect(@item).to be_valid
+     end
+
+     it  '商品の状態が選択される' do
+       @item.status_id
+       expect(@item).to be_valid
+     end
+
+     # 配送について
+     it  '配送料の負担' do
+       @item.delivery_charge_id
+       expect(@item).to be_valid
+     end
+
+     it '発送元の地域' do
+       @item.prefecture_id
+       expect(@item).to be_valid
+     end
+
+     it '発送までの日数' do
+       @item.sending_day_id
+       expect(@item).to be_valid
+     end
+
+     # 販売価格
+     it '価格が入力される' do
+       @item.price
+       expect(@item).to be_valid
+     end
 
     end
 
 
     # 異常テスト
     context '出品に失敗するとき' do
+
+      it 'imageが空では登録できない'do
+      end
+
+      it '商品名が空では登録できない'do
+      end
+
+      it  '商品説明が空では登録できない' do
+      end
+
+      it  'カテゴリーが空では登録できない' do
+      end
+
+      it  '商品の状態が空では登録できない' do
+      end
+
+      it  '発送元の地域が空では登録できない' do
+      end
+
+      it  '発送までの日数が空では登録できない' do
+      end
+
+      it '価格が空だと登録できない' do
+      end
+
+      it '全角数字では登録できない' do
+      end
+
+      it '半角英数混合では登録できない' do
+      end
+
+      it '半角英語だけでは登録できない' do
+      end
+
+      it '299円以下では登録できない' do
+      end
+
+      it '10,000,000以上では登録できない' do
+      end
 
     end
   end
