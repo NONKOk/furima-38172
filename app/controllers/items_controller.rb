44bb9022a44443c27073d7d97739305c56840fc6
NORIKOk <mun.7781.gon@gmail.com>
2022-07-21 19:32:32 +0900

    itemコントローラーのindexに追加の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.all.order('created_at DESC')
+    @item = Item.all.order('created_at DESC')
+    @items = @item.includes(:user)
   end
 
   def new
