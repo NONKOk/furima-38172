06514aec0d4ff8d3a726225d581a2196af4f60c8
NORIKOk <mun.7781.gon@gmail.com>
2022-07-23 18:45:48 +0900

    addressのマイグレーションファイルの修正

--- db/migrate/20220722130245_create_addresses.rb
+++ db/migrate/20220722130245_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
-    create_table :addressbooks do |t|
+    create_table :addresses do |t|
       t.references :order,foreign_key: true
       t.string :ppst_code,      null: false
       t.integer :prefecture_id, null: false
       t.string  :city,          null: false
       t.string  :house_num,     null: false
-      t.string  :building,
+      t.string  :building
       t.string  :phone_num,     null: false
 
       t.timestamps
