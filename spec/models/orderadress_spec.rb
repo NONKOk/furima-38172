eb7b166d52592925022038afb8f55ffd009eb18f
NORIKOk <mun.7781.gon@gmail.com>
2022-07-25 13:07:43 +0900

    sleepメソッドを追加

--- spec/models/orderadress_spec.rb
+++ spec/models/orderadress_spec.rb
@@ -3,12 +3,12 @@ equire 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '商品購入機能'
   before do
-    @user = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item)
-
-    @user.save
-    @item.save
-    @orderaddress = FactoryBot.build(:orderaddress,user_id: @user.id, item_id: @item.id)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    #@user.save	
+    #@item.save
+    @orderaddress = FactoryBot.build(:orderaddress, user_id: @user.id, item_id: @item.id)
+    sleep 0.1
   end
 
   context '内容に問題ない場合' do
