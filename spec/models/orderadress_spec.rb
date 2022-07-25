bfb6d37db09c387b2582a12def8eca0e81495dc9
NORIKOk <mun.7781.gon@gmail.com>
2022-07-25 11:59:01 +0900

    userとitemをBDに登録してそのIDを使用して＠orderaddressを作成するよう記述

--- spec/models/orderadress_spec.rb
+++ spec/models/orderadress_spec.rb
@@ -3,7 +3,12 @@ equire 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '商品購入機能'
   before do
-    @orderaddress = FactoryBot.build(:order_address)
+    @user = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
+
+    @user.save
+    @item.save
+    @orderaddress = FactoryBot.build(:orderaddress,user_id: @user.id, item_id: @item.id)
   end
 
   context '内容に問題ない場合' do
