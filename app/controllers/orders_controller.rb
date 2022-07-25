5aa988356bf0faece85ae12d2794ecd63b51f80d
NORIKOk <mun.7781.gon@gmail.com>
2022-07-25 15:39:35 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,14 +22,14 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:post_code,:prefecture_id,:city,:house_num,:building,:phone_num).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+    params.params.require(:order_address)permit(:price,:post_code,:prefecture_id,:city,:house_num,:building,:phone_num).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      amount: @item.price,  # 商品の値段
+      card: @orderaddress.token,    # カードトークン
       currency: 'jpy'                # 通貨の種類（日本円）
     )
   end
