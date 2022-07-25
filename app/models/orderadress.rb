cf035055f2b9730ec3f310cf68d3f1e45d37653b
NORIKOk <mun.7781.gon@gmail.com>
2022-07-25 20:45:39 +0900

    モデルの微修正

--- app/models/orderadress.rb
+++ app/models/orderadress.rb
@@ -2,7 +2,7 @@ class OrderAddress
 
   include ActiveModel::Model
   # orderテーブルとaddressbookテーブルに保存したいカラム名を指定
-  attr_accessor :postnum,:prefecture_id,:city,:banchi,:tower,:phone,:user_id,:item_id,:order_id,:token
+  attr_accessor :postnum,:prefecture_id,:city,:banchi,:tower,:phone,:user_id,:item_id,:token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
