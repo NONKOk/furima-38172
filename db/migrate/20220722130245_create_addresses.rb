fb0d98ef8d1c7c219114985c8a59b021d3b9d00c
NORIKOk <mun.7781.gon@gmail.com>
2022-07-27 21:36:58 +0900

    テーブル名の修正

--- db/migrate/20220722130245_create_addresses.rb
+++ db/migrate/20220722130245_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.references :order,foreign_key: true
-      t.string :ppst_code,      null: false
+      t.string :post_code,      null: false
       t.integer :prefecture_id, null: false
       t.string  :city,          null: false
       t.string  :house_num,     null: false
