d95e0040512738e7ef5b0f40d276784c4d89b445
NORIKOk <mun.7781.gon@gmail.com>
2022-07-28 19:44:24 +0900

    先ほどのと一緒

--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 2022_07_22_130245) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
