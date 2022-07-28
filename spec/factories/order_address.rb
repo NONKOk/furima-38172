46c8dfffabbd0f97ea13d8c7981c89e08abc367b
NORIKOk <mun.7781.gon@gmail.com>
2022-07-28 10:57:19 +0900

    birthdayになっていた建物のところをbuildingに修正

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     prefecture_id   { 2 }
     city            {'京都'}
     house_num       {'西京区桂1-1'}
-    birthday        {'桂ビル101'}
+    building        {'桂ビル101'}
     phone_num       {'09012345678'}
   end
 end
